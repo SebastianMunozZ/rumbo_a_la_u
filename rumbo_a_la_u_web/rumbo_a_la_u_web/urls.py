04d7767cedcf43896a4a8c20fb216c1a6a184206
kimateluna <ki.mateluna@duocuc.cl>
2024-06-11 21:54:30 -0400

    se suben cambios

--- rumbo_a_la_u_web/rumbo_a_la_u_web/urls.py
+++ rumbo_a_la_u_web/rumbo_a_la_u_web/urls.py
@@ -21,6 +21,9 @@ from .views import *
 from django.urls import path, include
 from .views_register import RegisterView
 
+from django.urls import path
+from .views import *
+
 urlpatterns = [
     path('', index, name='index'),
     path('admin/', admin.site.urls),
@@ -41,59 +44,64 @@ urlpatterns = [
     path('alumno-calendario', alumnocalendario, name="alumnocalendario"),
     path('alumno-configuraciones', alumnoconfiguraciones,
          name="alumnoconfiguraciones"),
-    path('alumno-cursosmatriculados',
-         alumnocursosmatriculados, name="alumnocursosmatriculados"),
+    path('alumno-cursosmatriculados', alumnocursosmatriculados,
+         name="alumnocursosmatriculados"),
     path('alumno-dashboard', alumnodashboard, name="alumnodashboard"),
-    path('alumno-historialpedidos',
-         alumnohistorialpedidos, name="alumnohistorialpedidos"),
+    path('alumno-historialpedidos', alumnohistorialpedidos,
+         name="alumnohistorialpedidos"),
     path('alumno-listadeseos', alumnolistadeseos, name="alumnolistadeseos"),
     path('alumno-miperfil', alumnomiperfil, name="alumnomiperfil"),
     path('alumno-misevaluaciones', alumnomisevaluaciones,
          name="alumnomisevaluaciones"),
     path('alumno-pregyresp', alumnopregyresp, name="alumnopregyresp"),
-    path('alumno-resenas', alumnoresenas name="alumnoresenas"),
+    path('alumno-resenas', alumnoresenas, name="alumnoresenas"),  # Corregido
 
     # Seccion Dashboard Profesor
-    path('profesor-anuncios', profesor-anuncios, name="profesor-anuncios"),
-    path('profesor-asignaciontareas', profesor - \
-         asignaciontareas, name="profesor-asignaciontareas"),
-    path('profesor-calendario', profesor - \
-         calendario, name="profesor-calendario"),
-    path('profesor-certificado', profesor - \
-         certificado, name="profesor-certificado"),
-    path('profesor-configuraciones', profesor - \
-         configuraciones, name="profesor-configuraciones"),
-    path('profesor-dashboard', profesor-configuraciones,
-         name="profesor-configuraciones"),
-    path('profesor-evaluaciones', profesor - \
-         evaluaciones, name="profesor-evaluaciones"),
-    path('profesor-miperfil', profesor-miperfil, name="profesor-miperfil"),
-    path('profesor-miperfil', profesor-miperfil, name="profesor-miperfil"),
-    path('profesor-miscursos', profesor-miscursos, name="profesor-miscursos"),
-    path('profesor-pregyresp', profesor-pregyresp, name="profesor-pregyresp"),
-    path('profesor-registro', profesor-registro, name="profesor-registro"),
-    path('profesor-reporteria', profesor - \
-         reporteria, name="profesor-reporteria"),
-    path('profesor-resenas', profesor-resenas, name="profesor-resenas"),
-    path('profesor-saldo', profesor-saldo, name="profesor-saldo"),
+    path('profesor-anuncios', profesor_anuncios,
+         name="profesor_anuncios"),  # Corregido
+    path('profesor-asignaciontareas', profesor_asignaciontareas,
+         name="profesor_asignaciontareas"),  # Corregido
+    path('profesor-calendario', profesor_calendario,
+         name="profesor_calendario"),  # Corregido
+    path('profesor-certificado', profesor_certificado,
+         name="profesor_certificado"),  # Corregido
+    path('profesor-configuraciones', profesor_configuraciones,
+         name="profesor_configuraciones"),  # Corregido
+    path('profesor-dashboard', profesor_dashboard,
+         name="profesor_dashboard"),  # Corregido
+    path('profesor-evaluaciones', profesor_evaluaciones,
+         name="profesor_evaluaciones"),  # Corregido
+    path('profesor-miperfil', profesor_miperfil,
+         name="profesor_miperfil"),  # Corregido
+    path('profesor-miscursos', profesor_miscursos,
+         name="profesor_miscursos"),  # Corregido
+    path('profesor-pregyresp', profesor_pregyresp,
+         name="profesor_pregyresp"),  # Corregido
+    path('profesor-registro', profesor_registro,
+         name="profesor_registro"),  # Corregido
+    path('profesor-reporteria', profesor_reporteria,
+         name="profesor_reporteria"),  # Corregido
+    path('profesor-resenas', profesor_resenas,
+         name="profesor_resenas"),  # Corregido
+    path('profesor-saldo', profesor_saldo, name="profesor_saldo"),  # Corregido
 
     # Seccion Perfiles Profesores
-    path('profesor-profile-biologia', profesor - \
-         profile-biologia, name="profesor-profile-biologia"),
-    path('profesor-profile-complectora', profesor - \
-         profile-complectora, name="profesor-profile-complectora"),
-    path('profesor-profile-fisica', profesor - \
-         profile-fisica, name="profesor-profile-fisica"),
-    path('profesor-profile-historia', profesor - \
-         profile-historia, name="profesor-profile-historia"),
-    path('profesor-profile-matematica', profesor - \
-         profile-matematica, name="profesor-profile-matematica"),
-    path('profesor-profile-quimica', profesor - \
-         profile-quimica, name="profesor-profile-quimica"),
+    path('profesor-profile-biologia', profesor_profile_biologia,
+         name="profesor_profile_biologia"),  # Corregido
+    path('profesor-profile-complectora', profesor_profile_complectora,
+         name="profesor_profile_complectora"),  # Corregido
+    path('profesor-profile-fisica', profesor_profile_fisica,
+         name="profesor_profile_fisica"),  # Corregido
+    path('profesor-profile-historia', profesor_profile_historia,
+         name="profesor_profile_historia"),  # Corregido
+    path('profesor-profile-matematica', profesor_profile_matematica,
+         name="profesor_profile_matematica"),  # Corregido
+    path('profesor-profile-quimica', profesor_profile_quimica,
+         name="profesor_profile_quimica"),  # Corregido
 
     # Seccion blog
     path('blog', blog, name="blog"),
-    path('blogdet', blogblogdet, name="blogblogdet"),
+    path('blogdet', blogdet, name="blogdet"),
     path('blogblogdet01', blogblogdet01, name="blogblogdet01"),
     path('blogblogdet02', blogblogdet02, name="blogblogdet02"),
     path('blogblogdet03', blogblogdet03, name="blogblogdet03"),
@@ -102,25 +110,25 @@ urlpatterns = [
 
     # Seccion Cursos
     path('cursos', cursos, name="cursos"),
-    path('curso-crear', curso-crear, name="curso-crear"),
-    path('cursos-biologia-organismoyambiente', cursos-biologia - \
-         organismoyambiente, name="cursos-biologia-organismoyambiente"),
-    path('cursos-complectora-evaluar', cursos - \
-         complectora-evaluar, name="cursos-complectora-evaluar"),
-    path('cursos-complectora-interpretar', cursos - \
-         complectora-interpretar, name="cursos-complectora-interpretar"),
-    path('cursos-complectora-localizar', cursos - \
-         complectora-localizar, name="cursos-complectora-localizar"),
-    path('cursos-fisica-mecanica', cursos-fisica - \
-         mecanica, name="cursos-fisica-mecanica"),
-    path('cursos-historia-ejehistoria', cursos - \
-         historia-ejehistoria, name="cursos-historia-ejehistoria"),
-    path('cursos-matematicas-algebrayfunciones', cursos - \
-         matematicas-algebrayfunciones, name="cursos-matematicas-algebrayfunciones"),
-    path('cursos-matematicas-numeros', cursos - \
-         matematicas-numeros, name="cursos-matematicas-numeros"),
-    path('cursos-quimica-estructuraatomica', cursos - \
-         quimica-estructuraatomica, name="cursos-quimica-estructuraatomica"),
+    path('curso-crear', curso_crear, name="curso_crear"),
+    path('cursos-biologia-organismoyambiente', cursos_biologia_organismoyambiente,
+         name="cursos_biologia_organismoyambiente"),  # Corregido
+    path('cursos-complectora-evaluar', cursos_complectora_evaluar,
+         name="cursos_complectora_evaluar"),  # Corregido
+    path('cursos-complectora-interpretar', cursos_complectora_interpretar,
+         name="cursos_complectora_interpretar"),  # Corregido
+    path('cursos-complectora-localizar', cursos_complectora_localizar,
+         name="cursos_complectora_localizar"),  # Corregido
+    path('cursos-fisica-mecanica', cursos_fisica_mecanica,
+         name="cursos_fisica_mecanica"),  # Corregido
+    path('cursos-historia-ejehistoria', cursos_historia_ejehistoria,
+         name="cursos_historia_ejehistoria"),  # Corregido
+    path('cursos-matematicas-algebrayfunciones', cursos_matematicas_algebrayfunciones,
+         name="cursos_matematicas_algebrayfunciones"),  # Corregido
+    path('cursos-matematicas-numeros', cursos_matematicas_numeros,
+         name="cursos_matematicas_numeros"),  # Corregido
+    path('cursos-quimica-estructuraatomica', cursos_quimica_estructuraatomica,
+         name="cursos_quimica_estructuraatomica"),  # Corregido
 
     # Seccion Header
     path('header', header, name="header"),
@@ -139,20 +147,17 @@ urlpatterns = [
     path('register/', RegisterView.as_view(), name='views_register'),
 
     # Seccion zoom
-    path('zoom-detalles-mat-algebrayfunciones', zoom-detalles-mat - \
-         algebrayfunciones, name="zoom-detalles-mat-algebrayfunciones"),
-    path('zoom-detalles-mat-geometria', zoom - \
-         detalles-mat-geometria, name="zoom-detalles-mat-geometria"),
-    path('zoom-detalles-mat-geometria', zoom - \
-         detalles-mat-geometria, name="zoom-detalles-mat-geometria"),
-    path('zoom-detalles-mat-probabilidadyestadistica', zoom - \
-         detalles-probabilidadyestadistica, name="zoom-detalles-probabilidadyestadistica"),
-    path('zoom-detalles-quim-estructuraatomica', zoom - \
-         detalles-quim-estructuraatomica, name="zoom-detalles-quim-estructuraatomica"),
-    path('zoom-detalles-quim-quimicaorganica', zoom - \
-         detalles-quim-quimicaorganica, name="zoom-detalles-quim-quimicaorganica"),
-    path('zoom-detalles-quim-quimicasyestequiometria', zoom - \
-         detalles-quim-quimicasyestequiometria, name="zoom-detalles-quim-quimicasyestequiometria"),
-    path('zoom-reuniones', zoomdetails, name="zoom-details"),
-
+    path('zoom-detalles-mat-algebrayfunciones', zoom_detalles_mat_algebrayfunciones,
+         name="zoom_detalles_mat_algebrayfunciones"),  # Corregido
+    path('zoom-detalles-mat-geometria', zoom_detalles_mat_geometria,
+         name="zoom_detalles_mat_geometria"),  # Corregido
+    path('zoom-detalles-mat-probabilidadyestadistica', zoom_detalles_mat_probabilidadyestadistica,
+         name="zoom_detalles_mat_probabilidadyestadistica"),  # Corregido
+    path('zoom-detalles-quim-estructuraatomica', zoom_detalles_quim_estructuraatomica,
+         name="zoom_detalles_quim_estructuraatomica"),  # Corregido
+    path('zoom-detalles-quim-quimicaorganica', zoom_detalles_quim_quimicaorganica,
+         name="zoom_detalles_quim_quimicaorganica"),  # Corregido
+    path('zoom-detalles-quim-quimicasyestequiometria', zoom_detalles_quim_quimicasyestequiometria,
+         name="zoom_detalles_quim_quimicasyestequiometria"),  # Corregido
+    path('zoom-reuniones', zoom_reuniones, name="zoom_reuniones"),  # Corregido
 ]
--- rumbo_a_la_u_web/rumbo_a_la_u_web/views.py
+++ rumbo_a_la_u_web/rumbo_a_la_u_web/views.py
@@ -1,61 +1,327 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 
 def index(request):
+    # Código para la vista de la página de inicio
     return render(request, 'index.html')
 
 
+def error(request):
+    # Código para la vista de la página de error 404
+    return HttpResponse("Error 404 - Página no encontrada")
+
+
 def sobrenosotros(request):
+    # Código para la vista de la sección "Sobre Nosotros"
     return render(request, 'sobrenosotros.html')
 
 
-def profesores(request):
-    return render(request, 'profesores.html')
-
-
-def pricing(request):
-    return render(request, 'pricing.html')
-
-
-def bloglist(request):
-    return render(request, 'blog-list.html')
-
-
-def blogdetails(request):
-    return render(request, 'blog-details.html')
-
-
-def zoommeeting(request):
-    return render(request, 'zoom-meeting.html')
-
-
-def zoomdetails(request):
-    return render(request, 'zoom-details.html')
-
-
-def event(request):
-    return render(request, 'event.html')
-
-
 def carro(request):
+    # Código para la vista del carrito de compras
     return render(request, 'carro.html')
 
 
-def login(request):
-    return render(request, 'login.html')
+def profesores(request):
+    # Código para la vista de la sección "Profesores"
+    return render(request, 'profesores.html')
 
 
-def registration(request):
-    return render(request, 'registro.html')
+def alumnocalendario(request):
+    # Código para la vista del calendario del alumno
+    return render(request, 'alumno-calendario.html')
 
 
-def cursocrear(request):
+def alumnoconfiguraciones(request):
+    # Código para la vista de las configuraciones del alumno
+    return render(request, 'alumno-configuraciones.html')
+
+
+def alumnocursosmatriculados(request):
+    # Código para la vista de los cursos matriculados del alumno
+    return render(request, 'alumno-cursosmatriculados.html')
+
+
+def alumnodashboard(request):
+    # Código para la vista del dashboard del alumno
+    return render(request, 'alumno-dashboard.html')
+
+
+def alumnohistorialpedidos(request):
+    # Código para la vista del historial de pedidos del alumno
+    return render(request, 'alumno-historialpedidos.html')
+
+
+def alumnolistadeseos(request):
+    # Código para la vista de la lista de deseos del alumno
+    return render(request, 'alumno-listadeseos.html')
+
+
+def alumnomiperfil(request):
+    # Código para la vista del perfil del alumno
+    return render(request, 'alumno-miperfil.html')
+
+
+def alumnomisevaluaciones(request):
+    # Código para la vista de las evaluaciones del alumno
+    return render(request, 'alumno-misevaluaciones.html')
+
+
+def alumnopregyresp(request):
+    # Código para la vista de preguntas y respuestas del alumno
+    return render(request, 'alumno-pregyresp.html')
+
+
+def alumnoresenas(request):
+    # Código para la vista de las reseñas del alumno
+    return render(request, 'alumno-resenas.html')
+
+
+def profesor_anuncios(request):
+    # Código para la vista de la sección de anuncios del profesor
+    return render(request, 'profesor-anuncios.html')
+
+
+def profesor_asignaciontareas(request):
+    # Código para la vista de la asignación de tareas del profesor
+    return render(request, 'profesor-asignaciontareas.html')
+
+
+def profesor_calendario(request):
+    # Código para la vista del calendario del profesor
+    return render(request, 'profesor-calendario.html')
+
+
+def profesor_certificado(request):
+    # Código para la vista del certificado del profesor
+    return render(request, 'profesor-certificado.html')
+
+
+def profesor_configuraciones(request):
+    # Código para la vista de las configuraciones del profesor
+    return render(request, 'profesor-configuraciones.html')
+
+
+def profesor_dashboard(request):
+    # Código para la vista del dashboard del profesor
+    return render(request, 'profesor-dashboard.html')
+
+
+def profesor_evaluaciones(request):
+    # Código para la vista de las evaluaciones del profesor
+    return render(request, 'profesor-evaluaciones.html')
+
+
+def profesor_miperfil(request):
+    # Código para la vista del perfil del profesor
+    return render(request, 'profesor-miperfil.html')
+
+
+def profesor_miscursos(request):
+    # Código para la vista de los cursos del profesor
+    return render(request, 'profesor-miscursos.html')
+
+
+def profesor_pregyresp(request):
+    # Código para la vista de preguntas y respuestas del profesor
+    return render(request, 'profesor-pregyresp.html')
+
+
+def profesor_registro(request):
+    # Código para la vista del registro del profesor
+    return render(request, 'profesor-registro.html')
+
+
+def profesor_reporteria(request):
+    # Código para la vista de la reportería del profesor
+    return render(request, 'profesor-reporteria.html')
+
+
+def profesor_resenas(request):
+    # Código para la vista de las reseñas del profesor
+    return render(request, 'profesor-resenas.html')
+
+
+def profesor_saldo(request):
+    # Código para la vista del saldo del profesor
+    return render(request, 'profesor-saldo.html')
+
+
+def profesor_profile_biologia(request):
+    # Código para la vista del perfil de biología del profesor
+    return render(request, 'profesor-profile-biologia.html')
+
+
+def profesor_profile_complectora(request):
+    # Código para la vista del perfil de complectora del profesor
+    return render(request, 'profesor-profile-complectora.html')
+
+
+def profesor_profile_fisica(request):
+    # Código para la vista del perfil de física del profesor
+    return render(request, 'profesor-profile-fisica.html')
+
+
+def profesor_profile_historia(request):
+    # Código para la vista del perfil de historia del profesor
+    return render(request, 'profesor-profile-historia.html')
+
+
+def profesor_profile_matematica(request):
+    # Código para la vista del perfil de matemática del profesor
+    return render(request, 'profesor-profile-matematica.html')
+
+
+def profesor_profile_quimica(request):
+    # Código para la vista del perfil de química del profesor
+    return render(request, 'profesor-profile-quimica.html')
+
+
+def blog(request):
+    # Código para la vista de la sección de blog
+    return render(request, 'blog.html')
+
+
+def blogdet(request):
+    # Código para la vista de la página de detalles del blog
+    return render(request, 'blogdet.html')
+
+
+def blogblogdet01(request):
+    # Código para la vista de la página 01 de detalles del blog
+    return render(request, 'blogblogdet01.html')
+
+
+def blogblogdet02(request):
+    # Código para la vista de la página 02 de detalles del blog
+    return render(request, 'blogblogdet02.html')
+
+
+def blogblogdet03(request):
+    # Código para la vista de la página 03 de detalles del blog
+    return render(request, 'blogblogdet03.html')
+
+
+def blogblogdet04(request):
+    # Código para la vista de la página 04 de detalles del blog
+    return render(request, 'blogblogdet04.html')
+
+
+def blogblogdet05(request):
+    # Código para la vista de la página 05 de detalles del blog
+    return render(request, 'blogblogdet05.html')
+
+
+def cursos(request):
+    # Código para la vista de la sección de cursos
+    return render(request, 'cursos.html')
+
+
+def curso_crear(request):
+    # Código para la vista de la creación de curso
     return render(request, 'curso-crear.html')
 
 
-def becomeinstructor(request):
-    return render(request, 'become-instructor.html')
+def cursos_biologia_organismoyambiente(request):
+    # Código para la vista de cursos de biología sobre organismos y ambiente
+    return render(request, 'cursos-biologia-organismoyambiente.html')
 
 
-def error(request):
-    return render(request, '404.html')
+def cursos_complectora_evaluar(request):
+    # Código para la vista de cursos de complectora sobre evaluación
+    return render(request, 'cursos-complectora-evaluar.html')
+
+
+def cursos_complectora_interpretar(request):
+    # Código para la vista de cursos de complectora sobre interpretación
+    return render(request, 'cursos-complectora-interpretar.html')
+
+
+def cursos_complectora_localizar(request):
+    # Código para la vista de cursos de complectora sobre localización
+    return render(request, 'cursos-complectora-localizar.html')
+
+
+def cursos_fisica_mecanica(request):
+    # Código para la vista de cursos de física sobre mecánica
+    return render(request, 'cursos-fisica-mecanica.html')
+
+
+def cursos_historia_ejehistoria(request):
+    # Código para la vista de cursos de historia sobre el eje historia
+    return render(request, 'cursos-historia-ejehistoria.html')
+
+
+def cursos_matematicas_algebrayfunciones(request):
+    # Código para la vista de cursos de matemáticas sobre álgebra y funciones
+    return render(request, 'cursos-matematicas-algebrayfunciones.html')
+
+
+def cursos_matematicas_numeros(request):
+    # Código para la vista de cursos de matemáticas sobre números
+    return render(request, 'cursos-matematicas-numeros.html')
+
+
+def cursos_quimica_estructuraatomica(request):
+    # Código para la vista de cursos de química sobre estructura atómica
+    return render(request, 'cursos-quimica-estructuraatomica.html')
+
+
+def header(request):
+    # Código para la vista de la sección de header
+    return render(request, 'header.html')
+
+
+def footer(request):
+    # Código para la vista de la sección de footer
+    return render(request, 'footer.html')
+
+
+def login(request):
+    # Código para la vista de la sección de login
+    return render(request, 'login.html')
+
+
+def planesdemembresia(request):
+    # Código para la vista de la sección de planes de membresía
+    return render(request, 'planesdemembresia.html')
+
+
+def registro(request):
+    # Código para la vista de la sección de registro
+    return render(request, 'registro.html')
+
+
+def zoom_detalles_mat_algebrayfunciones(request):
+    # Código para la vista de detalles de zoom para matemáticas - álgebra y funciones
+    return render(request, 'zoom-detalles-mat-algebrayfunciones.html')
+
+
+def zoom_detalles_mat_geometria(request):
+    # Código para la vista de detalles de zoom para matemáticas - geometría
+    return render(request, 'zoom-detalles-mat-geometria.html')
+
+
+def zoom_detalles_mat_probabilidadyestadistica(request):
+    # Código para la vista de detalles de zoom para matemáticas - probabilidad y estadística
+    return render(request, 'zoom-detalles-mat-probabilidadyestadistica.html')
+
+
+def zoom_detalles_quim_estructuraatomica(request):
+    # Código para la vista de detalles de zoom para química - estructura atómica
+    return render(request, 'zoom-detalles-quim-estructuraatomica.html')
+
+
+def zoom_detalles_quim_quimicaorganica(request):
+    # Código para la vista de detalles de zoom para química orgánica
+    return render(request, 'zoom-detalles-quim-quimicaorganica.html')
+
+
+def zoom_detalles_quim_quimicasyestequiometria(request):
+    # Código para la vista de detalles de zoom para química y estequiometría
+    return render(request, 'zoom-detalles-quim-quimicasyestequiometria.html')
+
+
+def zoom_reuniones(request):
+    # Código para la vista de detalles de zoom para reuniones
+    return render(request, 'zoom-reuniones.html')
