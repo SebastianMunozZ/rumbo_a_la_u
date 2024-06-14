cf0aaf7aeba8de4fb97a632644e67b55580dcfa9
Sebastian Muñoz <samunoz@rayensalud.com>
2024-06-13 22:55:11 -0400

    se agrega envío de correo al registrar

--- rumbo_a_la_u_web/rumbo_a_la_u_web/settings.py
+++ rumbo_a_la_u_web/rumbo_a_la_u_web/settings.py
@@ -18,6 +18,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.office365.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'rumboalau@outlook.com'
+EMAIL_HOST_PASSWORD = 'Bravenewworld15'
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
